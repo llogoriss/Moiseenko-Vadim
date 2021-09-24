56d3660f46f7c091cf0ee9274e4dddef8ff1365b
llogoriss <zedgaming@ya.ru>
2021-09-24 12:05:01 +0300

    Занятие 1

---
+++ Занятие 1/6.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+a=0
+x=int(input('Введите X='))
+y=int(input('Введите Y='))
+a=x
+x=y
+y=a
+print('X=',x,'Y=',y)
---
+++ Занятие 1/7.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+number = int(input('Введите значение number='))
+if number%2 == 0 :
+    print('Число number является чётным')
+else:
+    print('Число number является нечётным')
