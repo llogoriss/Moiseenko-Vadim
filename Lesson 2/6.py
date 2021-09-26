b25b48b88db17d7754183bbed36c217ae4c81c37
llogoriss <zedgaming@ya.ru>
2021-09-27 01:02:41 +0300

    .

--- Lesson 2/6.py
+++ Lesson 2/6.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+print('Введите четыре числа от 1 до 8')
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if (a + b + c + d) % 2 == 0:
+    print('Да')
+else:
+    print('Нет')
