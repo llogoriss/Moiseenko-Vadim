1fb3a8716c3230c313add69a588187011640f90d
llogoriss <zedgaming@ya.ru>
2021-10-08 12:34:25 +0300

    Готово

--- Lesson 3/4.py
+++ Lesson 3/4.py
@@ -2,5 +2,5 @@ s = 0
 n = int(input('Введите число N - '))
 for i in range(n):
     a = int(input('Введите число - '))
-    s = s + a
+    s += a
 print('Сумма всех чисел равна', s)
---
+++ Lesson 3/5.py
@@ -0,0 +1,5 @@
+sum = 0
+n = int(input('Введите число N: '))
+for i in range (1, n + 1):
+    sum += i**3
+print(sum)
