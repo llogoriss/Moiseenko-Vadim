08a089de8f10c24c46934e17e895a736afa9e24e
llogoriss <zedgaming@ya.ru>
2022-04-13 14:35:52 +0300

    .

---
+++ Osn_prog/Lesson 1/2.java
@@ -0,0 +1,6 @@
+public class Parametr {
+    public static void main(String[] args) {
+        System.out.println("Вы ввели "+ (args.length > 0? args.length + " параметров" : "Вы не передавали параметров"));
+    }
+}
+
